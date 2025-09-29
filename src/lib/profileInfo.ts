a1fc531c7f60ab177bbd3bb0ab11a218d2e0a558
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2025-09-29 12:30:51 +0600

    profile image uplode api done

--- src/lib/profileInfo.ts
+++ src/lib/profileInfo.ts
@@ -1,12 +1,13 @@
+import { ProfileResponse } from "@/types/userDataType"
 
 export async function uploadAvatar(file: File, token: string) {
   const formData = new FormData()
   formData.append("profileImage", file)
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/user/upload-avatar`,
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/setting/profile`,
     {
-      method: "PATCH",
+      method: "PUT",
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -19,5 +20,20 @@ export async function uploadAvatar(file: File, token: string) {
     throw new Error(resData.message || "Failed to upload image")
   }
 
+  return resData
+}
+
+
+export async function getProfile(token: string): Promise<ProfileResponse> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/setting/profile`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  })
+  const resData = await response.json()
+  if (!response.ok) {
+    throw new Error(resData.message || "Failed to get profile")
+  }
   return resData
 }
